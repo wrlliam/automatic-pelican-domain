ffb0ce0cb3e6f9fcfd888d3ec389ad9108ed85d1
William Banham <will_banh@icloud.com>
2025-09-15 20:27:18 +0100

    changes

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import Elysia from "elysia";
 import logixlysia from "logixlysia";
 
 export const app = new Elysia()
-  .get(`/`, ({ request, body }) => {
+  .post(`/`, ({ request, body }) => {
     console.log(request);
 
     return new Response(
